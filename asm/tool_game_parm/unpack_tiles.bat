25f5297d0f62769ca1bbca55a206495fcc9512f5
hansenhahn <hansen.hahn@gmail.com>
2018-07-09 21:03:24 -0300

    Tool para extração do gráfico Warning, em game_parm.bin

---
+++ asm/tool_game_parm/unpack_tiles.bat
@@ -0,0 +1,6 @@
+@echo off
+
+echo "Unpacking 004 tilesets"
+pypy tool_tilemap.py -m u -t "asm_game_parm_001\001_000.bin" -s "asm_game_parm_000\000.bin" -d "asm_game_parm_000\000_000_image.bin" -e 0 -c 4
+pypy tool_tilemap.py -m u -t "asm_game_parm_001\001_001.bin" -s "asm_game_parm_000\000.bin" -d "asm_game_parm_000\000_001_image.bin" -e 0 -c 4
+
